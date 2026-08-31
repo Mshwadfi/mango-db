04dad895bd6b882a327152068a43a109cd3c3ff0
Mshwadfi <mohamedalshwadfy24@gmail.com>
2026-08-31 17:09:57 +0300

    feat: disk manager

--- src/main.go
+++ src/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"mydb/src/storageLayer"
 )
 
 func main() {
+
+	dm, err := storageLayer.OpenDiskManager("./data/mydb.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer dm.Close()
+
+	pageId, err := dm.AllocatePage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	page := storageLayer.NewPage()
 
-	// slot1, _ := page.Insert([]byte("salam"))
-	// record1, _ := page.Get(slot1)
+	slot1, err := page.Insert([]byte("salam"))
 
-	for i := 0; i < 5000; i++ {
-		_, err := page.Insert([]byte("salam"))
-		if err != nil {
-			fmt.Printf("insert failed at i=%d: %v\n", i, err)
-			break
-		}
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	slot2, err := page.Insert([]byte("ya 3alam"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = dm.WritePage(pageId, page)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("page:", pageId)
+	fmt.Println("slot1:", slot1)
+	fmt.Println("slot2:", slot2)
 }
--- src/storageLayer/diskManager.go
+++ src/storageLayer/diskManager.go
@@ -12,9 +12,9 @@ type DiskManager struct {
 	numberOfPages uint16
 }
 
-var errorPageNotFound = errors.New("page not found")
+var ErrPageOutOfRange = errors.New("page ID out of range")
 
-func openDiskManager(path string) (*DiskManager, error) {
+func OpenDiskManager(path string) (*DiskManager, error) {
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
 
 	if err != nil {
@@ -30,4 +30,62 @@ func openDiskManager(path string) (*DiskManager, error) {
 		file:          file,
 		numberOfPages: uint16(info.Size() / PageSize),
 	}, nil
+}
+
+func (dm *DiskManager) AllocatePage() (uint16, error) {
+	dm.mu.Lock()
+	defer dm.mu.Unlock()
+
+	pageId := dm.numberOfPages
+	offset := int64(pageId) * PageSize // writeAt expects offset as int64
+
+	page := NewPage()
+	_, err := dm.file.WriteAt(page.Data, offset)
+
+	if err != nil {
+		return 0, err
+	}
+
+	dm.numberOfPages++
+	return pageId, nil
+}
+
+func (dm *DiskManager) ReadPage(pageId uint16) (*Page, error) {
+	dm.mu.Lock() // change it later to RWmutex for concurrent read, exclusive write
+	defer dm.mu.Unlock()
+
+	if pageId >= dm.numberOfPages {
+		return nil, ErrPageOutOfRange
+	}
+	buffer := make([]byte, PageSize)
+	offset := int64(pageId) * PageSize
+
+	_, err := dm.file.ReadAt(buffer, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Data: buffer}, nil
+}
+
+func (dm *DiskManager) WritePage(pageId uint16, p *Page) error {
+	dm.mu.Lock()
+	defer dm.mu.Unlock()
+
+	if pageId >= dm.numberOfPages {
+		return ErrPageOutOfRange
+	}
+
+	offset := int64(pageId) * PageSize
+	_, err := dm.file.WriteAt(p.Data, offset)
+
+	if err != nil {
+		return err
+	}
+
+	return dm.file.Sync()
+}
+
+func (dm *DiskManager) Close() error {
+	return dm.file.Close()
 }
